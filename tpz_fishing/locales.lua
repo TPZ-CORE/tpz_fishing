24ccd533f8260104d3e202564fd4f3fa5ed065a6
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-03 13:55:12 +0200

    Update locales.lua

--- tpz_fishing/locales.lua
+++ tpz_fishing/locales.lua
@@ -1,19 +1,23 @@
 Locales = {}
 
 Locales = {
-	['PREPARE_FISHING_ROD'] = "Prepare Fishing Rod",
-    ['THROW_HOOK']          = "Cast Fishing Rod",
-    ['HOOK_FISH']           = "HOOK",
-    ['RESET_CAST']          = "Reset Cast",
-    ['REEL_LURE']           = "Reel Lure",
-    ['REEL_IN']             = "Reel In",
-    ['KEEP_FISH']           = "Keep Fish",
-    ['THROW_FISH']          = "Throw Fish",
-    ['READY_TO_FISH']       = "Ready to fish!",
-    ['FISHING']             = "Fishing",
-    ['GET_THE_FISH']        = "Get the fish!",
-    ['FISH_NAME']           = "Name",
-    ['FISH_WEIGHT']         = "Weight",
+	['PREPARE_FISHING_ROD']                   = "Prepare Fishing Rod",
+    ['THROW_HOOK']                            = "Cast Fishing Rod",
+    ['HOOK_FISH']                             = "HOOK",
+    ['RESET_CAST']                            = "Reset Cast",
+    ['REEL_LURE']                             = "Reel Lure",
+    ['REEL_IN']                               = "Reel In",
+    ['KEEP_FISH']                             = "Keep Fish",
+    ['THROW_FISH']                            = "Throw Fish",
+    ['READY_TO_FISH']                         = "Ready to fish!",
+    ['FISHING']                               = "Fishing",
+    ['GET_THE_FISH']                          = "Get the fish!",
+    ['FISH_NAME']                             = "Name",
+    ['FISH_WEIGHT']                           = "Weight",
 
-    ['FISH_CATCHED']        = "You have found a %s",
+    ['FISH_CATCHED']                          = "You have found a %s",
+    ['FISH_LOST']                             = "You lost a %s, you couldn't carry it.",
+
+    ['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'] = "DevTools / Injection Cheat Found!",
+    ['DEVTOOLS_INJECTION_DETECTED']           = "You have been kicked due to cheating by using DevTools or Injection cheat.",
 }
